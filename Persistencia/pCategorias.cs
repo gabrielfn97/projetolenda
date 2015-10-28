c32349d808fe86d0cb05266f40946cdafeff7626
Gabrieldpaula2 <baiakel@hotmail.com>
2015-10-27 18:10:06 -0600

    CategoriadeFilmes

---
+++ Persistencia/pCategorias.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class pCategorias
+    {
+        String generos;
+
+
+        public void inserir(String generos)
+        {
+            String SQL = "INSERT INTO Categorias (generos) VALUES(" + generos + ")";
+            Conexao oConexao = new Conexao("SQLServer");
+            oConexao.executeNoQuery(SQL);
+            oConexao.fechaConexao();
+        }
+
+        public void alterar()
+        {
+
+        }
+
+        public void apagar()
+        {
+
+        }
+
+        public Object consultarTodos()
+        {
+            return 1;
+        }
+
+
+    }
+}
