e2e38f92915bd9c6da9dcfff3c7786fc36c30f7b
anac1 <ana.carolinna_s@outlook.com>
2015-11-05 13:35:31 -0200

    Add Teste3.cs

---
+++ Negocio/Teste3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    class Teste3
+    {
+        String nome;
+
+    }
+}
