c1b21cccd20ea968f953208ff80d76436f68bbb1
alexandremanzi <alexandre_wow_pba@hotmail.com>
2015-11-08 14:19:46 -0200

    classes de negocio e persistencia de cadestado

---
+++ Negocio/CadEstado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+
+namespace Negocio
+{
+    public class CadEstado
+    {
+        
+        String estado;
+        String sigla;
+
+        public void inserir( String estado, String sigla)
+        {
+            pCadEstado objCadEstado = new pCadEstado();
+            objCadEstado.inserir(estado,sigla);
+        }
+
+        public void alterar()
+        {
+
+        }
+
+        public void apagar()
+        {
+
+        }
+
+        public Object consultarTodos()
+        {
+            return 1;
+        }
+
+       
+
+       
+
+    }
+}
---
+++ Persistencia/pCadEstado.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class pCadEstado
+    {
+   
+        String estado;
+        String sigla;
+
+        public void inserir( String estado, String sigla)
+        {
+            String SQL = "INSERT INTO dbo.CadEstado( estado, sigla) VALUES(''" + estado + "','" + sigla + "')";
+            Conexao oConexao = new Conexao("SQLServer");
+            oConexao.executeNoQuery(SQL);
+            oConexao.fechaConexao();
+        }
+
+
+        public void alterar()
+        {
+
+        }
+
+        public void apagar()
+        {
+
+        }
+
+        public Object consultarTodos()
+        {
+            return 1;
+        }
+
+
+    }
+}
