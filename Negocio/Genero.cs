fff1095e7e0032899f794a81f66a68282a571a72
felipealvessa <felipe199820102010@hotmail.com>
2015-11-12 15:28:38 -0200

    add Genero

---
+++ Negocio/Genero.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+
+namespace Negocio
+{
+    public class Genero
+    {
+        String generos;
+
+        public void inserir(String generos)
+        {
+            pGenero objGenero = new pGenero();
+            objGenero.inserir(generos);
+        }
+
+        public void alterar()
+        {
+
+        }
+
+        public void deletar()
+        {
+
+        }
+
+        public Object consultarTodos()
+        {
+            return 1;
+        }
+
+
+    }
+}
