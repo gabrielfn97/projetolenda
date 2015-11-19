aae84efea7c512e4ffff4ad5f7d046d42d31cc48
hector1001 <hector_10as@outlook.com>
2015-11-19 15:03:52 -0200

    Criei VotacaoFilme em Negocio

---
+++ Negocio/VotacaoFilme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+
+namespace Negocio
+{
+    public class VotacaoFilme
+    {
+        String idLogin;
+        String idFilme;
+        String qtdEstrela;
+
+        public void inserir(String idLogin, String idFilme, String qtdEstrela)
+        {
+            pVotacaoFilme objVotacaoFilme = new pVotacaoFilme();
+            objVotacaoFilme.inserir(idLogin, idFilme, qtdEstrela);
+        }
+
+        public void alterar()
+        {
+
+        }
+
+        public void apagar()
+        {
+
+        }
+
+        public Object consultarTodos()
+        {
+            return 1;
+        }
+
+    }
+}
