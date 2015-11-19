2e8d80769a6c60e780539495f8b88a6fee736688
hector1001 <hector_10as@outlook.com>
2015-11-19 15:07:06 -0200

    pVotacaoFilme em Persistencia

---
+++ Persistencia/pVotacaoFilme.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class pVotacaoFilme
+    {
+        String idLogin;
+        String idFilme;
+        String qtdEstrela;
+
+        public void inserir(String idLogin, String idFilme, String qtdEstrela)
+        {
+            String SQL = "INSERT INTO VotacaoFilme(idLogin, idFilme, qtdEstrela) VALUES(" + idLogin + ",'" + idFilme + "','" + qtdEstrela + "')";
+            Conexao oConexao = new Conexao("SQLServer");
+            oConexao.executeNoQuery(SQL);
+            oConexao.fechaConexao();
+        }
+
+        public void alterar()
+        {
+
+        }
+
+        public void apagar()
+        {
+
+        }
+
+        public Object consultarTodos()
+        {
+            return 1;
+        }
+
+    }
+}
