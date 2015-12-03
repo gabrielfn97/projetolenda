535552c607c92cbc3472e1727910e05b9484c5c3
gabrielfn97 <gabrielfn97@outlook.com>
2015-12-03 14:20:44 -0200

    inserir botao

--- WebAppExercicio/frmCadCategoria.aspx.designer.cs
+++ WebAppExercicio/frmCadCategoria.aspx.designer.cs
@@ -65,5 +65,14 @@ namespace WebAppExercicio {
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Controle Button2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
