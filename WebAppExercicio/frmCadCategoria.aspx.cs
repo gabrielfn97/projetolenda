3cb924059646eeed5d7b4a8249f7b3e9f5a94a15
gabrielfn97 <gabrielfn97@outlook.com>
2015-12-03 16:13:34 -0200

    teste

--- WebAppExercicio/frmCadCategoria.aspx.cs
+++ WebAppExercicio/frmCadCategoria.aspx.cs
@@ -3,49 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 using Negocio;
-
-
+using System.Data;
 
 
 namespace WebAppExercicio
 {
     public partial class frmCadCategoria : System.Web.UI.Page
     {
+        DataSet lista;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                Categoria oCategoria = new Categoria();
+                lista = oCategoria.consultarTodos();
 
+                if (lista != null)
+                {
+                    gdvCadCategoria.DataSource = lista;
+                    gdvCadCategoria.DataMember = "Tabela";
+                    gdvCadCategoria.DataBind();
+                }
+            }
         }
 
+        protected void gdvCadCategoria_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            GridViewRow row = gdvCadCategoria.SelectedRow;
+            txtdescricao.Text = Server.HtmlDecode(row.Cells[1].Text);
+            txtNdoFilme.Text = Server.HtmlDecode(row.Cells[2].Text);
+        }
 
-        protected void btnInserir_Click(object sender, EventArgs e)
+        protected void btnInserir_Click1(object sender, EventArgs e)
         {
             Categoria objCategoria = new Categoria();
             objCategoria.inserir(txtdescricao.Text, txtNdoFilme.Text);
-
         }
 
-        protected void btnAlterar_Click(object sender, EventArgs e)
+        protected void btnAlterar_Click1(object sender, EventArgs e)
         {
             Categoria objCategoria = new Categoria();
             objCategoria.alterar(txtdescricao.Text, Convert.ToInt32(txtNdoFilme.Text));
             Response.Redirect(Request.RawUrl);
         }
 
-        protected void btnApagar_Click(object sender, EventArgs e)
+        protected void btnApagar_Click1(object sender, EventArgs e)
         {
             Categoria objCategoria = new Categoria();
             objCategoria.apagar(txtdescricao.Text, Convert.ToInt32(txtNdoFilme.Text));
             Response.Redirect(Request.RawUrl);
         }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-             
-      
 
     }
 }
