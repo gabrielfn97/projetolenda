17dd54b2fd444b089c279cb56d9b6b52dfee104f
anac1 <ana.carolinna_s@outlook.com>
2015-12-10 14:56:02 -0200

    includ

--- WebAppExercicio/frmGeneros.aspx.cs
+++ WebAppExercicio/frmGeneros.aspx.cs
@@ -5,14 +5,28 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Negocio;
+using System.Data;
 
 namespace WebAppExercicio
 {
     public partial class frmcategoriasfilmes : System.Web.UI.Page
     {
+        DataSet lista;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                Genero oGenero = new Genero();
+                lista = oGenero.consultarTodos();
 
+                if (lista != null)
+                {
+                    gdvGeneros.DataSource = lista;
+                    gdvGeneros.DataMember = "Tabela";
+                    gdvGeneros.DataBind();
+                }
+            }
         }
 
         protected void btnInserir_Click(object sender, EventArgs e)
@@ -33,6 +47,12 @@ namespace WebAppExercicio
             Genero objGenero = new Genero();
             objGenero.apagar(txtGeneros.Text);
             Response.Redirect(Request.RawUrl);
+        }
+
+        protected void gdvGeneros_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow row = gdvGeneros.SelectedRow;
+            txtGeneros.Text = Server.HtmlDecode(row.Cells[1].Text);
         }
     }
 }
