4d334aaeae3aa054600a2feee5371ea56a15dc7e
gabrielfn97 <gabrielfn97@outlook.com>
2015-12-10 13:56:52 -0200

    Adição de Usuarios

---
+++ Negocio/Usuarios.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+
+namespace Negocio
+{
+    public class Usuarios
+    {
+        String email;
+        String usuario;
+        String senha;
+
+        public void inserir(String email, String usuario, String senha)
+        {
+            pUsuarios objCadLogin = new pUsuarios();
+            objCadLogin.inserir(email, usuario, senha);
+        }
+
+        public void alterar(String email, String usuario, String senha)
+        {
+            pUsuarios objCadLogin = new pUsuarios();
+            objCadLogin.alterar(email, usuario, senha);
+        }
+
+        public void apagar(String email, String usuario, String senha)
+        {
+            pUsuarios objCadLogin = new pUsuarios();
+            objCadLogin.apagar(email, usuario, senha);
+        }
+
+        public Object consultarTodos()
+        {
+            pUsuarios objCadLogin = new pUsuarios();
+            return objCadLogin.consultarTodos();
+        }
+
+        public String logar(String usuario, String senha)
+        {
+            pUsuarios objCadLogin = new pUsuarios();
+            objCadLogin.logar(usuario, senha);
+            return "";
+        }
+    }
+}
