ab69af2f777bc86143a288b583bd1a76776ab1d3
PedroAmerico <pedroamerico2213@gmail.com>
2016-01-09 09:45:06 -0200

    Dados Complementares

---
+++ Negocio/DadosComplementares.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistencia;
+using System.Data;
+
+
+namespace Negocio
+{
+    public class DadosComplementares
+    {
+        String idDadosComplementares;
+        String endereco;
+        String telefone;
+        String bairro;
+        String estadoCivil;
+        String ImgPerfilUrl;
+        String DataNascimento;
+        String sexo;
+    
+
+        public void inserir(String idDadosComplementares,String endereco,String telefone,String bairro,String estadoCivil,String ImgPerfilUrl,String DataNascimento,String sexo)
+        {
+            pDadosComplementares objDadosComplementares = new pDadosComplementares();
+            objDadosComplementares.inserir(idDadosComplementares, endereco, telefone, bairro, estadoCivil, ImgPerfilUrl, DataNascimento, sexo);
+        }
+
+        public void alterar(String idDadosComplementares, String endereco, String telefone, String bairro, String estadoCivil, String ImgPerfilUrl, String DataNascimento, String sexo)
+        {
+            pDadosComplementares objDadosComplementares = new pDadosComplementares();
+            objDadosComplementares.alterar(idDadosComplementares, endereco, telefone, bairro, estadoCivil, ImgPerfilUrl, DataNascimento, sexo);
+        }
+
+        public void apagar(String idDadosComplementares, String endereco, String telefone, String bairro, String estadoCivil, String ImgPerfilUrl, String DataNascimento, String sexo)
+        {
+            pDadosComplementares objDadosComplementares = new pDadosComplementares();
+            objDadosComplementares.apagar(idDadosComplementares, endereco, telefone, bairro, estadoCivil, ImgPerfilUrl, DataNascimento, sexo);
+        }
+
+        public DataSet consultarTodos()
+        {
+            pDadosComplementares objDadosComplementares = new pDadosComplementares();
+            return objDadosComplementares.consultarTodos();
+        }
+
+
+    }
+}
