97c9043a7fdef85106ff73dba969a2c9e079c5ec
gabrielfn97 <gabrielfn97@outlook.com>
2016-01-14 15:41:50 -0200

    Adição reTodasCriticas (Corrigido Erros)

---
+++ WebAppExercicio/reTodasCriticas.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="reTodasCriticas.aspx.cs" Inherits="WebAppExercicio.reTodasCriticas" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Lista de Todas as Criticas<br />
+        <br />
+        <asp:Table ID="tabelaCriticas" runat="server">
+        </asp:Table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebAppExercicio/reTodasCriticas.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+using System.Data;
+
+namespace WebAppExercicio
+{
+    public partial class reTodasCriticas : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            carregarTabelaDataSet();
+        }
+
+        private void carregarTabelaDataSet()
+        {
+            CadCritica objListarTodasNoticias = new CadCritica();
+            DataSet dtListarTodosFilmes = objListarTodasNoticias.consultarTodos();
+
+            for (int linha = 0; linha <= dtListarTodosFilmes.Tables["Tabela"].Rows.Count - 1; linha++)
+            {
+                TableRow tRow = new TableRow();
+                tabelaCriticas.Rows.Add(tRow);
+
+                for (int coluna = 0; coluna <= 3; coluna++)
+                {
+                    TableCell tCell = new TableCell();
+                    tCell.Text = Convert.ToString(dtListarTodosFilmes.Tables["Tabela"].Rows[linha][coluna]);
+                    tRow.Cells.Add(tCell);
+                }
+            }
+        }
+    }
+}
